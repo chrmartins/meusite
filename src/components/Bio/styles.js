6cec739fc5f0f34491cd9204cf511a98c9e21d73
Christian Martins <cmts2010@gmail.com>
2020-03-03 16:51:09 -0300

    margens da bio alterada

--- src/components/Bio/styles.js
+++ src/components/Bio/styles.js
@@ -2,21 +2,23 @@ import styled from 'styled-components'
 
 export const  TextBio = styled.div`
 
-  margin: 20px;
+margin: 50px;
   font-family: 'Spartan', sans-serif;
  
   h1{
+    margin-top: 40px;
     text-align: center;
     font-size: 15px;
   }
   
   
   p{
-    margin-top: 40px;
+    
+    margin-top: 30px;
     text-align: justify;
     margin-left: auto;
     margin-right: auto;
-    width: 40em;
+    width: ;
     color: #607D8B;
     font-size: 17px;
     line-height: 30px;
