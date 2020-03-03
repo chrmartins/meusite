095a8673300c80836dce0e65aa6701ae87728fb7
Christian Martins <cmts2010@gmail.com>
2020-03-03 17:15:15 -0300

    margens

--- src/components/Bio/styles.js
+++ src/components/Bio/styles.js
@@ -2,23 +2,23 @@ import styled from 'styled-components'
 
 export const  TextBio = styled.div`
 
-margin: 50px;
+  /* margin: 20px; */
   font-family: 'Spartan', sans-serif;
  
   h1{
-    margin-top: 40px;
+    margin: 40px;
     text-align: center;
     font-size: 15px;
   }
   
   
   p{
-    
-    margin-top: 30px;
+    padding: 15px;
     text-align: justify;
+    margin-top: 10px;
     margin-left: auto;
     margin-right: auto;
-    width: 90%;
+    width: 60em;
     color: #607D8B;
     font-size: 17px;
     line-height: 30px;
